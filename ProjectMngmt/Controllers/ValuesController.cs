588a0b5f6cfa7eecc89684592e7abec730e206ca
newtrendstech <jzhang@newtrendstech.com>
2013-07-06 16:51:04 +0800

    a git testing

--- ProjectMngmt/Controllers/ValuesController.cs
+++ ProjectMngmt/Controllers/ValuesController.cs
@@ -15,7 +15,7 @@ namespace ProjectMngmt.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/values/5 
         public string Get(int id)
         {
             return "value";
