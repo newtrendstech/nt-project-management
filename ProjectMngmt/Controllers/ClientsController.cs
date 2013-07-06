f498aa8ccc271d25ddb185b1f87105d7f60be6bb
newtrendstech <jzhang@newtrendstech.com>
2013-07-06 16:43:38 +0800

    Add the controller

---
+++ ProjectMngmt/Controllers/ClientsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.OData;
+using System.Data.Entity.Infrastructure;
+
+using ProjectMngmt.Models;
+
+namespace ProjectMngmt.Controllers
+{
+    public class ClientsController : EntitySetController<Client, int>
+    {
+        ProjectMngmtContext _context = new ProjectMngmtContext();
+
+        public override IQueryable<Client> Get()
+        {
+            return _context.Clients;
+        }
+
+        protected override Client CreateEntity(Client entity)
+        {
+            _context.Clients.Add(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        protected override Client UpdateEntity(int key, Client update)
+        {
+            if (!_context.Clients.Any(c => c.ID == key))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Clients.Attach(update);
+            _context.Entry(update).State = System.Data.EntityState.Modified;
+            _context.SaveChanges();
+
+            return update;
+        }
+
+        public override void Delete(int key)
+        {
+            Client client = _context.Clients.FirstOrDefault(c => c.ID == key);
+            if (client == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            _context.Clients.Remove(client);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ ProjectMngmt/Models/ProjectMngmtContextInitializer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+
+namespace ProjectMngmt.Models
+{
+    public class ProjectMngmtContextInitializer 
+        : DropCreateDatabaseIfModelChanges<ProjectMngmtContext>
+    {
+        protected override void Seed(ProjectMngmtContext context)
+        {            
+            base.Seed(context);
+        }
+    }
+}
